4bb48d9ceacb91ad1a955bb4118e4c959b2d3fa4
ziyu <4a7g0119@stust.edu.tw>
2022-04-24 01:30:47 +0800

    Initial Commit

---
+++ music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  music
+//
+//  Created by 黃子瑜 on 2022/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
